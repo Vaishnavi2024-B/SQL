57ab0b7a9a6da70aa5b425d6dfadc36cd04d09fa
Vaishnavi2024-B <vaishnavi123.xworkz@gmail.com>
2024-06-27 00:21:56 +0530

    Assesment 26-06-24

---
+++ Assesment1.sql
@@ -0,0 +1,4 @@
+CREATE  DATABASE project;
+use project;
+CREATE TABLE Airport_details( id int,  a_name varchar(20),arrival_time bigint,departure_time bigint,price bigint);
+CREATE TABLE Cricket_team(id int,t_name varchar(30),no_of_players varchar(10),captain varchar(40),coach varchar(50));
